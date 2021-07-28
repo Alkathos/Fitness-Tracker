f97dc70fa983f8fa22bf4692181549c49f4b30fd
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-07-28 08:39:47 -0400

    Changed folder structure and updated links to js and css

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,13 @@ const app = require('../server');
 const path = require ('path');
 
 app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/pages/exercise.html"));
   });
 
-  app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
+app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/pages/stats.html"));
+  });
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/pages/index.html"));
   });
