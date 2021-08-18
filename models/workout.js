a0d3f870bde4b4521579c84227b5a09b7838816b
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-08-18 17:39:54 -0400

    Added index.js and day schema to models

--- models/workout.js
+++ models/workout.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+
+    day: {
+      type: Date,
+      default: Date.now
+    },
+
     exercises: [
         {
           type: {
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,14 +1,39 @@
-const app = require('../server');
-const path = require ('path');
+const db = require("../models");
 
-app.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/pages/exercise.html"));
-  });
-
-app.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/pages/stats.html"));
-  });
-
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/pages/index.html"));
-  });
+module.exports = function(app) {
+    // App.get to pull up info for the workouts page
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({}).then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+    })
+    // App.get to pull up info for the range page
+    app.get("/api/workouts/range", ({}, res) => {
+      db.Workout.find({}).then((dbWorkout) => {
+        res.json(dbWorkout);
+      }).catch(err => {
+        res.status(400).json(err);
+      });
+    });
+    // App.post to submit new completed workouts
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+          });
+      });
+      // App.put to update workouts by MongoDB _id value and update the exercsise body
+      app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
+        });
+    });
+};
